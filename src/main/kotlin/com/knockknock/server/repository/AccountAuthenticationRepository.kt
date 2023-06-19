1b82acdf562a20e9f7f57e3649a092f39f897022
ko-inseoklee <tjrkd222@gmail.com>
2023-06-19 23:54:12 +0900

    jpql에는 limit이 없어서 오버로딩으로 처리 (테스트 필요)

--- src/main/kotlin/com/knockknock/server/repository/AccountAuthenticationRepository.kt
+++ src/main/kotlin/com/knockknock/server/repository/AccountAuthenticationRepository.kt
@@ -1,6 +1,8 @@
 package com.knockknock.server.repository;
 
 import com.knockknock.server.entity.account.AccountAuthentication
+import org.springframework.data.domain.PageRequest
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
@@ -13,6 +15,11 @@ interface AccountAuthenticationRepository : JpaRepository<AccountAuthentication,
 
     fun countDistinctByPhoneNoAndExpireTimeGreaterThanEqual(phoneNo: String, expireTime: LocalDateTime): Long
 
-    @Query(value = "select auth from AccountAuthentication as auth where auth.email = ?1 order by auth.expireTime desc ")
-    fun findAuthCodeByEmail(email: String): AccountAuthentication
+    @Query(value = "select auth from AccountAuthentication as auth where auth.email = ?1 order by auth.expireTime desc")
+    fun findAuthCodeByEmail(email: String, pageble: Pageable): List<AccountAuthentication>
+
+    fun findAuthCodeByEmail(email: String): AccountAuthentication {
+        var result =  findAuthCodeByEmail(email, PageRequest.of(0,1));
+        return result[0]
+    }
 }
